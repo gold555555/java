7e4b155d72e6d6aec44127adda67d71be0a39bd5
gold555555 <goldstart.2112@gmail.com>
2025-12-19 22:35:42 +0700

    input name (String)

---
+++ Inputtutorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Inputtutorial {
+    public static void main(String[] args) {
+        //รับค่า
+        System.out.print("input your name : ");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine() ;
+        
+
+        System.out.println(name);
+    }
+}
