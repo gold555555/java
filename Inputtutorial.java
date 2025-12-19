622f6d6d399a1eb9514fe00eecc91516f0a69a93
gold555555 <goldstart.2112@gmail.com>
2025-12-20 02:30:25 +0700

    input tutorail

--- Inputtutorial.java
+++ Inputtutorial.java
@@ -6,8 +6,11 @@ public class Inputtutorial {
         System.out.print("input your name : ");
         Scanner sc=new Scanner(System.in);
         String name=sc.nextLine() ;
-        
-
         System.out.println(name);
+
+        System.out.print("Year of birth ");
+        Scanner pc=new Scanner(System.in);
+        int x = pc.nextInt();
+        System.out.print(x);
     }
 }
