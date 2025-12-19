6f0df72b62d57b337f8e838ab88d142908478780
gold555555 <goldstart.2112@gmail.com>
2025-12-20 01:57:43 +0700

    add tutorial

--- Variable.java
+++ Variable.java
@@ -3,8 +3,8 @@ public class Variable {
     public static void main(String[] args) {
         //นิยามตัวแปร
         //global variable
-        final int a= 15,b ,c ;
-        float x ;
+        final int a= 15 ;
+        float x = 3.145f;
         double y ;
         //a =5 ;
         System.out.println("value of a is "+a);
@@ -12,5 +12,22 @@ public class Variable {
             int num = 2; // num คือ local variable
             System.out.println("num is : "+num);
         }
+        
+        // เล้ก ---> ใหญ่
+        int num1 = 10;
+        double numDouble = num1;
+        System.out.println("int to double : "+numDouble);
+        
+        //ใหญ๋ --> เล็ก 
+        double numDouble2 = 10.0 ;
+        int numInt = (int)numDouble2;
+        System.out.println("double to int : "+numInt);
+
+        String b="100" ;
+        int c ;
+        //string to int 
+        c=Integer.parseInt(b);
+        c=c+50 ;
+        System.out.println("string to int : "+c);
     }
 }
