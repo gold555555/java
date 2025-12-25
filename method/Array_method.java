05173d948b8b2e72eec5975fe2b1c1d2da8a6578
gold555555 <goldstart.2112@gmail.com>
2025-12-26 03:45:27 +0700

    Array input output and some file class

--- method/Array_method.java
+++ method/Array_method.java
@@ -4,18 +4,26 @@
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n =sc.nextInt();
-            int [] number = new int[n] ;
-            for (int i = 0; i < n; i++) {
-                number[i] = sc.nextInt();
-            }
+            
+            int [] number = inputarr(n);
             outputarr(number);
             
+            
+        }
+        static int[] inputarr(int n){
+            Scanner kb = new Scanner(System.in);
+            int [] arr = new int [n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = kb.nextInt();
+                
+            }
+            return arr ;
         }
         //ถ้าใช้ void ไม่ต้องใช้ return 
-        static int outputarr(int [] arr ){
+        static void outputarr(int [] arr ){
             for (int i = 0; i < arr.length; i++) {
                 System.out.println("num "+arr[i]);
             }
-            return 0 ;
+            
         }
     }
--- method/Method1.java
+++ method/Method1.java
@@ -2,6 +2,11 @@ package method;
 
 public class Method1 {
     public static void main(String[] args) {
-        
+        display();display();display();
     }
+    //การสร้าง method
+    static void display(){
+        System.out.println("hello world");
+    }
+    
 }
